7249cf4db3f3798be512124b26287718283366b3
rniradiel <rniradiel@gmail.com>
2024-05-22 16:44:30 +0800

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -7,4 +9,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
